a772c2dfa734c4fb8547c56f81650e0307f4ee52
Archi Sen <archi.sen@tier5.in>
2024-06-25 12:11:25 +0530

    slider added and sectionOne test animation changed

--- src/components/ReviewsSection.jsx
+++ src/components/ReviewsSection.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const reviews = [
@@ -22,7 +25,7 @@ const reviews = [
 
 const ReviewCard = ({ name, review }) => {
   return (
-    <div className="frosted-glass shadow-lg rounded-lg p-6 my-4">
+    <div className="review-card shadow-lg rounded-lg p-6 my-4">
       <h3 className="text-2xl font-semibold text-blue-500">{name}</h3>
       <p className="mt-4 text-cyan-600">{review}</p>
     </div>
@@ -30,18 +33,46 @@ const ReviewCard = ({ name, review }) => {
 };
 
 const ReviewsSection = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0",
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
-    <section id='section' className="py-12 bg-gray-100 flex flex-col items-center">
+    <section id='section' className="pb-12 bg-gray-100 flex flex-col items-center">
       <div className="container mx-auto px-6">
         <h2 className="text-4xl font-bold text-blue-500 mb-8 text-center">Patient Reviews</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+        <Slider {...settings}>
           {reviews.map((review, index) => (
             <ReviewCard key={index} name={review.name} review={review.review} />
           ))}
-        </div>
+        </Slider>
       </div>
     </section>
   );
 };
 
-export default ReviewsSection
+export default ReviewsSection;
--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import baseImg from '../assets/images/image1.jpg';
+import { Animate } from 'react-simple-animate';
 
 const SectionOne = ({ onLoad }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
+  const [textVisible, setTextVisible] = useState(false);
 
   useEffect(() => {
     if (imageLoaded) {
       onLoad();
+      setTimeout(() => {
+        setTextVisible(true);
+      }, 500); // Delay to start the fade-in effect
     }
   }, [imageLoaded, onLoad]);
 
@@ -23,8 +28,19 @@ const SectionOne = ({ onLoad }) => {
         <>
           <div className="absolute inset-0 bg-black opacity-50 z-20"></div>
           <div className="relative z-30 text-center">
-            <h1 className="text-white text-5xl font-bold">Dr. Ranjan Kamilya</h1>
-            <p className="text-white text-2xl mt-4"> Pioneering Orthopaedic Surgeon and Eastern India's First Robotic Knee Replacement Specialist.</p>
+            <h1 className={`text-white text-6xl font-bold ${textVisible ? 'fade-in' : 'hidden'}`}>Dr. Ranjan Kamilya</h1>
+            {textVisible && (
+              <Animate
+                play={textVisible}
+                duration={1}
+                start={{ opacity: 0, transform: 'translateY(20px)' }}
+                end={{ opacity: 1, transform: 'translateY(0)' }}
+              >
+                <p className="text-white text-2xl mt-4 typing">
+                  Pioneering Orthopaedic Surgeon and Eastern India's First Robotic Knee Replacement Specialist.
+                </p>
+              </Animate>
+            )}
           </div>
         </>
       )}
